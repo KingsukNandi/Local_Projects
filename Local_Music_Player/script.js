9812dd25b2823ae1f15f5612ae1e41a81878dfd3
Kingsuk Nandi <kingsuk24nandi@gmail.com>
2024-06-04 22:04:36 +0530

    Update: Add minor styling changes to library Display .mp3 files in library section and set proper styling with overflow properties

--- Local_Music_Player/script.js
+++ Local_Music_Player/script.js
@@ -1,21 +1,49 @@
 let songs = [];
+var currSong;
+const jsmediatags = window.jsmediatags;
+
 async function generateSongList() {
   let songsHTML = await fetch(
     "http://127.0.0.1:5500/Projects/Local_Music_Player/songs/"
   );
+
   let songsHTMLText = await songsHTML.text();
-  console.log(songsHTMLText);
+
   let div = document.createElement("div");
+
   div.innerHTML = songsHTMLText;
+
   let songLinks = div.getElementsByTagName("a");
+
   songs = [];
-  for (const iterator of songLinks) {
-    if (iterator.href.endsWith(".mp3")) {
+
+  let index = 0;
+
+  for (const song of songLinks) {
+    if (song.href.endsWith(".mp3")) {
       let newSong = {
-        title: iterator.getAttribute("title"),
-        url: iterator.href,
+        title: song.getAttribute("title"),
+        url: song.href,
       };
+
       songs.push(newSong);
+
+      let songPlaceholder = document
+        .querySelector(".libraryList")
+        .getElementsByTagName("ol")[0];
+
+      songPlaceholder.innerHTML =
+        songPlaceholder.innerHTML +
+        `<li><div class="title">${newSong.title}</div><div class="artist">${newSong.artist}</div></li>`;
+
+      songPlaceholder
+        .getElementsByTagName("li")
+        [index].addEventListener("dblclick", function () {
+          currSong = new Audio(newSong.url);
+          currSong.play();
+        });
+
+      ++index;
     }
   }
   console.log(songs);
