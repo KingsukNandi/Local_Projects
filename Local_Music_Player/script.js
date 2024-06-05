33a166559ec61ccd460cac2cf36d161a1edd4730
Kingsuk Nandi <kingsuk24nandi@gmail.com>
2024-06-05 16:13:48 +0530

    Update: Add playability to songs list Add play & pause control in song control bar Fix: Fix song overlapping and last listed song error

--- Local_Music_Player/script.js
+++ Local_Music_Player/script.js
@@ -1,5 +1,5 @@
 let songs = [];
-var currSong;
+var currSong = new Audio();
 const jsmediatags = window.jsmediatags;
 
 async function generateSongList() {
@@ -15,9 +15,7 @@ async function generateSongList() {
 
   let songLinks = div.getElementsByTagName("a");
 
-  songs = [];
-
-  let index = 0;
+  // songs = []; // this sets an empty list of songs everytime the songs folder is modified
 
   for (const song of songLinks) {
     if (song.href.endsWith(".mp3")) {
@@ -25,6 +23,15 @@ async function generateSongList() {
         title: song.getAttribute("title"),
         url: song.href,
       };
+
+      let tag = await displayMetadata(newSong);
+      const data = tag.tags.picture.data;
+      const format = tag.tags.picture.format;
+      let base64string = "";
+      for (let i = 0; i < data.length; i++) {
+        base64string += String.fromCharCode(data[i]);
+      }
+      let url = console.log(tag);
 
       songs.push(newSong);
 
@@ -34,16 +41,21 @@ async function generateSongList() {
 
       songPlaceholder.innerHTML =
         songPlaceholder.innerHTML +
-        `<li><div class="title">${newSong.title}</div><div class="artist">${newSong.artist}</div></li>`;
-
-      songPlaceholder
-        .getElementsByTagName("li")
-        [index].addEventListener("dblclick", function () {
-          currSong = new Audio(newSong.url);
-          currSong.play();
-        });
-
-      ++index;
+        `<li class="flex">
+          <div>
+            <!-- <img src=url(data:${format};base64,${window.btoa(
+          base64string
+        )})> --> 
+          </div>
+          <div>
+            <div class="title">
+              ${tag.tags.title}
+            </div>
+            <div class="artist">
+              ${tag.tags.artist}
+            </div>
+          </div>
+        </li>`;
     }
   }
   console.log(songs);
@@ -56,14 +68,43 @@ async function setEvent(songsHTML) {
       .getElementsByClassName("libraryList")[0]
       .getElementsByTagName("li")
       [i].addEventListener("dblclick", function () {
-        currSong = new Audio(songs[i].url);
+        currSong.src = songs[i].url;
         currSong.play();
+        superControl.src = "assets/pauseCircle.svg";
+        console.log(i);
       });
+    console.log(i);
   }
+  superControl.addEventListener("click", function () {
+    if (currSong.paused) {
+      currSong.play();
+      superControl.src = "assets/pauseCircle.svg";
+    } else {
+      currSong.pause();
+      superControl.src = "assets/playCircle.svg";
+    }
+  });
   return songsHTML;
 }
 async function main() {
   let songsHTML = await generateSongList();
-  songsHTML = await setEvent();
+  songsHTML = await setEvent(songsHTML);
 }
 main();
+function displayMetadata(newSong) {
+  return new Promise((resolve, reject) => {
+    jsmediatags.read(newSong.url, {
+      onSuccess: function (tag) {
+        // console.log(tag.tags.title);
+        // console.log(tag.tags.artist);
+        // console.log(tag.tags.album);
+        // console.log(tag.tags.genre);
+        resolve(tag);
+      },
+      onError: function (error) {
+        console.log(`error`);
+        reject(error);
+      },
+    });
+  });
+}
